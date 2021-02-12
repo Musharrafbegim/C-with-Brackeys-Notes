45076f6ecef7ef36ec1733ddf40db8a60d16758c
Musharrafbegim <70473841+Musharrafbegim@users.noreply.github.com>
2021-02-12 16:35:05 +0500

    Update SWITCHSTATEMENTS.cs

--- SWITCHSTATEMENTS.cs
+++ SWITCHSTATEMENTS.cs
@@ -8,16 +8,43 @@ class mainClass
       int num01 = Generator.Next(1, 35);
       int num02 = Generator.Next(1, 6);
       
-       Console.WriteLine("What is" + num01 " divided by " + num02 " ? ");
+       Console.WriteLine("What is" + num01 " times " + num02 "?");
        
        int answer = Convert.ToInt32 (Console.ReadLine());
        
        if(answer == num01 / num02) {
         Console.WriteLine("Good job! You are welcome!");
+         int responseIndex = Generator.Next(1, 4);
+         switch(responseIndex){
+             case 1:
+             Console.WriteLine("I am proud of you!");
+             break;
+           case 2:
+             Console.WriteLine("Well done, man");
+             break;
+             
+             default:
+             Console.WriteLine("Attabooooy!!!!!!");
+             break;
       }else {
+        int diff Math.Abs (answer - (num01 * num02));
+           if (diff == 1){
+            Console.WriteLine("Gosh that was close")
+           }else if (diff <= 10){
+            Console.WriteLine("You can definitely do better!")
+           }else{
+            Console.WriteLine("You are not even close....")
+           }
+         }
         Console.WriteLine("Come on! it is a simple math!!!")
       }
       
       Console.ReadKey();
     {
 }
+    
+//Homework 
+    
+    
+    
+    
